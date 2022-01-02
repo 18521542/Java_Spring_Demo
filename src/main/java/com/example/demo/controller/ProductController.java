8a36b2ab386482070d1012bee33131199cab3747
TrongUIT <tranduytrong2000@gmail.com>
2022-01-03 04:52:09 +0700

    Annotation: PathVariable - use this to get param in URL, + Define findById - api

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,13 +4,15 @@ import com.example.demo.model.Product;
 import com.example.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/api/hello")
+@RequestMapping(path = "/api/Product")
 public class ProductController {
 
     @Autowired
@@ -19,5 +21,10 @@ public class ProductController {
     @GetMapping("")
     List<Product> Hello(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Product> findProductById(@PathVariable Long id){
+        return repository.findById(id);
     }
 }
