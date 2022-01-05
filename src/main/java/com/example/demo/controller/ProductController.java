9ce9bf538969499a6deb40be20c91dadd58bf113
TrongUIT <tranduytrong2000@gmail.com>
2022-01-06 03:14:50 +0700

    add Post API

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -4,10 +4,9 @@ import com.example.demo.model.CustomResponse;
 import com.example.demo.model.Product;
 import com.example.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,5 +35,12 @@ public class ProductController {
         catch (RuntimeException e){
             return new CustomResponse("failed", "Product not found!", null);
         }
+    }
+
+    @PostMapping
+    ResponseEntity<CustomResponse> insertProduct(@RequestBody Product newProduct){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new CustomResponse("OK", "Product Created!", repository.save(newProduct)));
     }
 }
